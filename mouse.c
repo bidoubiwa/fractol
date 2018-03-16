89860cce52d48af71fa5eda3f40d9de1b124c2c3
Pablo FAUST <pfaust@e2r1p7.42.fr>
2018-03-16 11:23:49 +0100

    changement de fractal au clic

--- mouse.c
+++ mouse.c
@@ -6,7 +6,7 @@
 /*   By: cvermand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 13:45:57 by cvermand          #+#    #+#             */
-/*   Updated: 2018/03/15 18:58:54 by cvermand         ###   ########.fr       */
+/*   Updated: 2018/03/16 11:22:37 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,21 @@ int			check_zone(int x, int y, t_env *env)
 	return (0);
 }
 
+void		switch_screens(int button, int zone, t_env *env)
+{
+	t_fractal	*tmp;
+
+	if (button == 1)
+	{
+		//dprintf(1, "zone = %d\n", zone);
+		tmp = env->screen[0]->fractal;
+		env->screen[0]->fractal = env->screen[zone - 1]->fractal;
+		env->screen[zone - 1]->fractal = tmp;
+		clear_and_redraw(env);
+		//dprintf(1, "jusqu'ici tout va bien\n");
+	}
+}
+
 int			mouse_hook(int button, int x, int y, t_env *env)
 {
 	int		zone;
@@ -33,6 +48,8 @@ int			mouse_hook(int button, int x, int y, t_env *env)
 	zone = check_zone(x, y, env);
 	if (zone == 1)
 		zoom(button, x, y, env);
+	if (zone != 1)
+		switch_screens(button, zone, env);
 	return (0);
 }
 
